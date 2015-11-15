178bc7fc666e81cdfe50a224dc54a246724fdbad
angelalin2004 <adl040@ucsd.edu>
2015-11-15 01:38:34 -0800

    fixed window scrolling, added drag handles and stacking (forgot to add last time)

--- js/index.js
+++ js/index.js
@@ -1,3 +1,7 @@
+$(window).scroll(function() {
+    scroll(0,0);
+});
+/*
 $(".img").click(function(event){
    if ($(this).hasClass("top")) {}
    else {
@@ -8,6 +12,10 @@ $(".img").click(function(event){
      $(this).removeClass("under");
      $(this).addClass("top");
      $(this).css("z-index", z);
-   }
-$(".img").draggable();  
+   }  
 });
+*/
+$( ".img" ).draggable({ stack: ".img" });
+$("#resume").draggable({ handle: "#resume-handle" });
+$("#readme").draggable({ handle: "#readme-handle" });
+$("#contact").draggable({ handle: "#contact-handle" });
